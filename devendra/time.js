45f2958378c2dd9ad66e85f4b27171ccb655969e
Devendra2872 <141264718+Devendra2872@users.noreply.github.com>
2023-08-04 11:36:53 +0000

    time and date

---
+++ devendra/time.js
@@ -0,0 +1,11 @@
+//let mytime = new Date()
+//console.log(mytime .toTimeString());
+
+//console.log(mytime.toISOString());
+
+let mydate = new Date(2023,1,22);
+console.log(mydate.toDateString());
+
+let newdate = new Date()
+//console.log(newdate.getHours());
+console.log(newdate.getUTCMonth());
